30de2f2c989fd0633d10e1411386ff5286e15223
Ido Strassberg <>
2022-09-30 12:41:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+
+intent = discord.Intents.default()
+intent.members = True
+intent.message_content = True
+
+client = discord.Client(intents=intent)
+
+@client.event
+async def on_ready():
+  print('We Have logged in as: {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('!hi'):
+    await message.channel.send('Hello!')
+
+client.run(os.environ['TOKEN'])
